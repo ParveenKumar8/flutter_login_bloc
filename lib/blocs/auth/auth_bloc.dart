62c37f81e62107996be357bfb4929d430a3a115c
“ParveenKumar8” <parveenkumar1101@gmail.com>
2024-02-21 13:23:24 +0530

    Implement the BlocConsumer and signout functionality.

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -6,31 +6,49 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        String email = event.email;
-        String password = event.password;
+    on<AuthLoginRequested>(_onAuthLoginRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
+  void _onAuthLoginRequested(
+    AuthLoginRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      String email = event.email;
+      String password = event.password;
 
-        if (email.isEmpty || password.isEmpty) {
-          return emit(AuthError("Enter required fields."));
-        }
-        final bool emailValid = RegExp(
-                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-            .hasMatch(email);
-        if (!emailValid) {
-          return emit(AuthError("Enter valid email address."));
-        }
-        if (password.length < 6) {
-          emit(AuthError("Password length should be more than 6 characters."));
-        }
-        await Future.delayed(const Duration(seconds: 3), () {
-          print("3 seconds passed.");
-        });
-        emit(AuthSuccess('$email - $password'));
-      } catch (e) {
-        emit(AuthError(e.toString()));
+      if (email.isEmpty || password.isEmpty) {
+        return emit(AuthError("Enter required fields."));
       }
-    });
+      final bool emailValid = RegExp(
+              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+          .hasMatch(email);
+      if (!emailValid) {
+        return emit(AuthError("Enter valid email address."));
+      }
+      if (password.length < 6) {
+        emit(AuthError("Password length should be more than 6 characters."));
+      }
+      await Future.delayed(const Duration(seconds: 3), () {
+        print("3 seconds passed.");
+        return emit(AuthSuccess('$email - $password'));
+      });
+    } catch (e) {
+      emit(AuthError(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(
+      AuthLogoutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 3), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      print(e.toString());
+    }
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_responsive_login_ui/blocs/auth/auth_bloc.dart';
+import 'package:flutter_responsive_login_ui/login_screen.dart';
+import 'package:flutter_responsive_login_ui/widgets/gradient_button.dart';
 
 class HomeScreen extends StatelessWidget {
   final String successMsg;
@@ -10,8 +14,35 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello!!! Encrypted Data is $successMsg."),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthInitial) {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+                (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Hello!!! Encrypted Data is $successMsg."),
+                GradientButton(
+                    onPressed: () {
+                      context.read<AuthBloc>().add(AuthLogoutRequested());
+                    },
+                    buttonName: "Sign Out")
+              ],
+            ),
+          );
+        },
       ),
     );
   }
